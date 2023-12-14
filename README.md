84ad176d2e59bec1b3b307f93126fd53fecd9942
guoshouqing001 <guoshouqing001@ke.com>
2023-12-14 14:46:07 +0800

    add Bulma css framework

--- README.md
+++ README.md
@@ -22,3 +22,35 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### Publish vue project to Github Pages
+To publish a Vue.js project from VSCode to GitHub Pages, follow these steps:
+
+Initialize a Git repository in your project folder (if not already initialized): Open the terminal in VSCode and run git init.
+
+Commit your project files to Git: Use git add . to stage your files and git commit -m "Initial commit" to make an initial commit.
+
+Create a GitHub repository: Go to GitHub and create a new repository for your project.
+
+Link your local repository to GitHub: Run the following commands, replacing <username> and <repository> with your GitHub username and repository name:
+
+git remote add origin https://github.com/<username>/<repository>.git
+git branch -M main
+git push -u origin main
+Build your Vue.js project: In your VSCode terminal, run the command to build your Vue.js project. For example: npm run build. This will generate a dist folder with your production-ready code.
+
+Install the gh-pages package: Run npm install --save gh-pages to install the GitHub Pages package.
+
+Configure your package.json: Add the following lines to your package.json file to specify the GitHub Pages deployment settings:
+```
+    "homepage": "https://<username>.github.io/<repository>/",
+    "scripts": {
+    "deploy": "gh-pages -d dist"
+    }
+```
+
+Deploy to GitHub Pages: Run npm run deploy to deploy your project to GitHub Pages.
+
+Access your project: Your Vue.js project should now be accessible at https://<username>.github.io/<repository>/.
+
+Make sure your GitHub repository is public, and you have the necessary permissions to deploy to GitHub Pages.
--- src/components/data.vue
+++ src/components/data.vue
@@ -1,48 +1,76 @@
 <template>
   <div class="content-wrapper">
-    <h1>List of works by Vincent van Gogh</h1>
-    <!-- 搜索框内容 -->
-    <div class="search-box">
-      <select v-model="selectedYear" id="year-select">
-        <option value="">时间</option>
-        <option v-for="year in yearOptions" :key="year" :value="year">{{ year }}年</option>
-      </select>
-      <input type="text" id="search-input" v-model="searchText" @keyup.enter="handleEnter" placeholder="作品关键词">
-      <button id="search-button" @click="filterData">搜索</button>
-    </div>
-    <div v-if="dataLoaded" class="grid-container">
-      <figure v-for="artwork in paginatedData" :key="artwork.catNo" class="grid-item">
-        <div class="image-figcaption-wrapper">
-          <div class="image-container">
-            <img v-if="artwork.imageUrl" :src="artwork.imageUrl" :alt="artwork.title">
-          </div>
-          <div class="figcaption-container">
-            <figcaption>
-              <div class="title">{{ artwork.title }}</div>
-              <div class="year">{{ artwork.year }}</div>
-            </figcaption>
-          </div>
+    <section class="hero is-small">
+      <div class="hero-body">
+        <p class="title">
+          List of works by Vincent van Gogh        </p>
+        <p class="subtitle">
+                  </p>
+      </div>
+    </section>
+    <h1></h1>
+    <!-- 搜索框 -->
+    <div class="field has-addons column is-half is-offset-one-quarter">
+      <div class="control">
+        <div class="select">
+          <select v-model="selectedYear">
+            <option value="">时间</option>
+            <option v-for="year in yearOptions" :key="year" :value="year">{{ year }}年</option>
+          </select>
         </div>
-      </figure>
-    </div>
-    <div v-else>Loading data...</div>
-    <div class="pagination">
-      <!-- 分页导航 -->
-      <div class="pagination">
-        <button @click="currentPage = 1">首页</button>
-        <button v-for="page in pagesToShow" :key="page" :class="{ current: page === currentPage }"
-          @click="currentPage = page">
-          {{ page }}
-        </button>
-        <button @click="currentPage = totalPages">尾页</button>
+      </div>
+      <div class="control is-expanded">
+        <input class="input" type="text" v-model="searchText" @keyup.enter="handleEnter" placeholder="作品关键词">
+      </div>
+      <div class="control">
+        <button class="button is-info" @click="filterData">搜索</button>
       </div>
     </div>
+    <!--内容-->
+    <div v-if="dataLoaded" class="columns is-multiline is-variable is-6">
+      <div v-for="artwork in paginatedData" :key="artwork.catNo" class="column is-6-mobile is-4-tablet is-3-desktop">
+        <div class="card">
+          <div class="card-image">
+            <figure class="image">
+              <img v-if="artwork.imageUrl" :src="artwork.imageUrl" :alt="artwork.title">
+            </figure>
+          </div>
+        </div>
+
+        <div class="content align-bottom">
+          <h5>{{ artwork.title }}</h5>
+          <p class="subtitle is-6">{{ artwork.year }}年</p>
+        </div>
+      </div>
+
+    </div>
+    <div v-else>Loading data...</div>
+    <nav class="pagination is-rounded column is-half is-offset-one-quarter" role="navigation" aria-label="pagination"
+      v-if="totalPages > 1">
+      <ul class="pagination-list">
+        <li><a class="pagination-link" @click="changePage(1)" :class="{ 'is-current': currentPage === 1 }"
+            aria-label="Goto page 1">1</a></li>
+        <!-- 如果当前页码距离首页有间隔，显示省略号 -->
+        <li v-if="currentPage > 3"><span class="pagination-ellipsis">&hellip;</span></li>
+        <li v-for="page in pagesToShow" :key="page">
+          <a class="pagination-link" @click="changePage(page)" :class="{ 'is-current': page === currentPage }"
+            :aria-label="'Goto page ' + page">{{ page }}</a>
+        </li>
+        <!-- 如果当前页码距离尾页有间隔，显示省略号 -->
+        <li v-if="currentPage < totalPages - 2"><span class="pagination-ellipsis">&hellip;</span></li>
+        <li v-if="totalPages > 1"><a class="pagination-link" @click="changePage(totalPages)"
+            :class="{ 'is-current': currentPage === totalPages }" :aria-label="'Goto page ' + totalPages">{{ totalPages
+            }}</a></li>
+      </ul>
+    </nav>
+
   </div>
 </template>
 
 
 <script>
 import axios from 'axios';
+import 'bulma/css/bulma.css';
 
 export default {
   name: 'MyComponent',
@@ -64,11 +92,23 @@ export default {
     },
     pagesToShow() {
       let pages = [];
-      let startPage = Math.max(this.currentPage - 5, 1);
-      let endPage = Math.min(startPage + 9, this.totalPages);
+      let startPage = Math.max(this.currentPage - 2, 2); // 从第2页开始显示
+      let endPage = Math.min(startPage + 4, this.totalPages - 1); // 最多显示5个页码，不包括第1页和最后一页
+
+      if (this.currentPage === 1) {
+        startPage = 2; // 如果是第一页，从第2页开始显示
+        endPage = Math.min(6, this.totalPages - 1); // 显示后面5个页码
+      }
+
+      if (this.currentPage === this.totalPages) {
+        endPage = this.totalPages - 1; // 如果是最后一页，显示到倒数第二页
+        startPage = Math.max(this.totalPages - 5, 2); // 显示前面5个页码
+      }
+
       for (let i = startPage; i <= endPage; i++) {
         pages.push(i);
       }
+
       return pages;
     },
     paginatedData() {
@@ -78,9 +118,11 @@ export default {
   },
   methods: {
     filterData() {
+
       this.dataContent = this.allData.filter(item => {
+        item.title
         return (this.selectedYear === '' || item.year === this.selectedYear) &&
-          (!this.searchText || item.title.includes(this.searchText));
+          (!this.searchText || String(item.title).toLowerCase().includes(this.searchText.toLowerCase()));
       });
     },
     getUniqueYears(data) {
@@ -94,10 +136,15 @@ export default {
     },
     handleEnter() {
       this.filterData();
+    },
+    changePage(newPage) {
+      if (newPage >= 1 && newPage <= this.totalPages) {
+        this.currentPage = newPage;
+      }
     }
   },
   created() {
-    axios.get('/artworks.json')
+    axios.get('artworks.json')
       .then(response => {
         this.allData = response.data;
         this.filterData(); // 应用初始过滤
@@ -114,11 +161,30 @@ export default {
 
 <style>
 .content-wrapper {
-  width: 66.67%;
-  /* 页面宽度的 3/2 */
+  width: 60%;
   margin: 0 auto;
-  /* 水平居中 */
+}
 
+.card {
+  box-shadow: none;
+  /* Use this to remove the shadow */
+}
+
+.column {
+  display: flex;
+  flex-wrap: wrap;
+  /* 使卡片在一行放不下时换行 */
+  justify-content: center;
+  /* 水平居中 */
+  align-items: center;
+  /* 垂直居中 */
+}
+
+.align-bottom {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  /* 垂直对齐到底部 */
 }
 
 /* 媒体查询 - 在小屏幕下显示两列 */
@@ -127,223 +193,5 @@ export default {
     width: 100%;
     /* 页面宽度为100%，适用于小屏幕 */
   }
-
-  .grid-item {
-    width: calc(50% - 10px);
-    /* 移动端显示两列，减去间距 */
-  }
 }
-
-.grid-container {
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
-  /* 创建一个灵活的多列布局 */
-  grid-gap: 10px;
-  /* 网格间距 */
-}
-
-.image-figcaption-wrapper {
-  display: flex;
-  /* 应用 Flexbox 布局 */
-  flex-direction: column;
-  /* 垂直排列子元素 */
-  min-height: 300px;
-  /* 设置固定的最小高度 */
-}
-
-.image-container {
-  flex-grow: 1;
-  /* 允许图片容器填充可用空间 */
-  display: flex;
-  align-items: center;
-  /* 垂直居中图片 */
-  justify-content: center;
-  /* 水平居中图片 */
-  margin-bottom: 10px;
-  /* 在图片容器底部添加间距 */
-
-}
-
-.grid-item img {
-  max-width: 100%;
-  /* 图片最大宽度 */
-  max-height: 100%;
-  /* 图片最大高度 */
-  display: block;
-}
-
-.figcaption-container {
-  flex-grow: 0;
-  /* 防止文本部分占据额外空间 */
-}
-
-h1 {
-  margin-bottom: 20px;
-  /* 你可以根据需要调整间距大小 */
-  align-items: center;
-  /* 垂直居中 */
-  justify-content: center;
-  /* 水平居中 */
-}
-
-.title,
-.year {
-  text-align: center;
-}
-
-.title {
-  font-size: 16px;
-  /* 较大的字体大小 */
-  font-family: 'Georgia', serif;
-  /* 圆润的字体 */
-  margin-bottom: 10px;
-  /* 底部添加间距 */
-  font-weight: bold;
-  /* 加粗字体 */
-}
-
-.year {
-  font-size: 14px;
-  /* 较小的字体大小 */
-  font-family: Arial, sans-serif;
-  /* 标准字体 */
-}
-
-.pagination {
-  margin-top: 30px;
-  /* 与上面容器的间距 */
-  display: flex;
-  /* 使用 Flexbox 布局 */
-  align-items: center;
-  /* 垂直居中 */
-  justify-content: center;
-  /* 水平居中 */
-}
-
-.pagination button {
-  align-items: center;
-  background: transparent;
-  border: 2px solid #666;
-  /* 添加边框 */
-  border-radius: 4px;
-  color: #333;
-  /* 根据需要调整颜色 */
-  display: inline-flex;
-  font-family: var(--met-sans), sans-serif;
-  /* 使用备用字体 */
-  font-size: 1em;
-  font-weight: 500;
-  justify-content: center;
-  padding: 8px 12px;
-  /* 添加一些内边距 */
-  cursor: pointer;
-  /* 鼠标悬停时显示指针 */
-  margin-right: 6px;
-  /* 添加按钮之间的间距 */
-}
-
-.pagination button.current {
-  background-color: #666;
-  /* 当前页面按钮的背景颜色 */
-  color: white;
-  border-color: #666;
-  /* 当前页面按钮的边框颜色 */
-
-}
-
-.pagination button:hover {
-  background-color: #ddd;
-  /* 鼠标悬停时按钮的背景颜色 */
-  border-color: #ddd;
-  /* 鼠标悬停时按钮的边框颜色 */
-
-}
-
-.search-box {
-  display: flex;
-  align-items: center;
-  background-color: #fff;
-  /* Adjust the background color as needed */
-  border: 1px solid #ccc;
-  /* Adjust the border color as needed */
-  border-radius: 4px;
-  padding: 5px;
-  /* Adjust the padding as needed */
-  margin-bottom: 20px;
-  /* Space below the search box */
-  width: auto;
-  /* Adjust the width as needed, or set a max-width */
-  max-width: 600px;
-  /* Example max-width */
-  margin-left: auto;
-  /* Centers the search box if within a container */
-  margin-right: auto;
-  /* Centers the search box if within a container */
-}
-
-#year-select {
-  border: 1px solid #ccc;
-  /* Give the dropdown a border */
-  margin-right: 10px;
-  /* Space between select and input */
-  padding: 10px;
-  border-radius: 4px;
-  position: relative;
-  background-size: 12px;
-  /* Adjust size as needed */
-  -moz-appearance: none;
-  /* Remove default styling in Firefox */
-  -webkit-appearance: none;
-  /* Remove default styling in Chrome and Safari */
-  appearance: none;
-  cursor: pointer;
-}
-
-#search-input {
-  flex-grow: 1;
-  border: 1px solid #ccc;
-  /* Give the input a border */
-  padding: 10px;
-  margin-right: 10px;
-  /* Space between input and button */
-  max-width: 300px;
-  /* Set a max-width for the input field */
-}
-
-#search-button {
-  padding: 10px 20px;
-  border-radius: 4px;
-  cursor: pointer;
-  background-color: white;
-  color: black;
-  /* 默认文字颜色 */
-  border: 1px solid #ccc;
-}
-
-/* Optional: add some hover effects */
-#search-button:hover {
-  background-color: #666;
-  /* Darker shade for hover effect */
-}
-
-/* Hide the focus outline to match the styling in the image */
-#year-select:focus,
-#search-input:focus,
-#search-button:focus {
-  outline: none;
-}
-
-/* Responsive to the resizing of the window */
-@media (max-width: 600px) {
-  .search-box {
-    flex-direction: column;
-  }
-
-  #year-select,
-  #search-input,
-  #search-button {
-    margin-right: 0;
-    margin-bottom: 10px;
-    /* Space between elements when stacked */
-  }
-}</style>
+</style>
